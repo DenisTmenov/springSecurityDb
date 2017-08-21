4dc1174de517b832d88f0b7a0af21d006a677e70
DenisTmenov <denis.tmenov@gmail.com>
2017-08-21 12:18:01 +0300

    UserController

---
+++ src/main/java/com/denis/springsecuritydb/controller/UserController.java
@@ -0,0 +1,46 @@
+package com.denis.springsecuritydb.controller;
+
+import com.denis.springsecuritydb.model.User;
+import com.denis.springsecuritydb.service.SecurityService;
+import com.denis.springsecuritydb.service.UserService;
+import com.denis.springsecuritydb.validator.UserValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private SecurityService securityService;
+
+    @Autowired
+    private UserValidator userValidator;
+
+    @RequestMapping(value = "/registration", method = RequestMethod.GET)
+    public String registration(Model model) {
+        model.addAttribute("userForm", new User());
+
+        return "registration";
+    }
+
+    @RequestMapping(value = "/registration", method = RequestMethod.POST)
+    public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult, Model model) {
+        userValidator.validate(userForm, bindingResult);
+
+        if (bindingResult.hasErrors()) {
+            return "registration";
+        }
+
+        userService.save(userForm);
+
+        securityService.autoLogin(userForm.getUsername(), userForm.getConfirmPassword());
+
+        return "redirect:/welcome";
+    }
+}
